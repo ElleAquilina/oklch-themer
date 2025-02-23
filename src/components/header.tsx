7fc715efb4664e56db7f10242492049b572c7614
eliah <eliah.aquilina@gmail.com>
2025-02-22 23:04:40 -0600

    Remove vite app defaults, add header

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <>
+            <header className='flex flex-row w-full justify-between'>
+                <div>
+                    <h4>TailwindCSS OKLCH Color Generator</h4>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import App from '@/app/app.tsx'
+import '@/styles/global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './app/app.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
