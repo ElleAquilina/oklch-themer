5be688e8800fd53707199fcedc125904a85ac656
eliah <eliah.aquilina@gmail.com>
2025-03-18 16:48:01 -0500

    Display card layout update

--- src/components/oklch-display.tsx
+++ src/components/oklch-display.tsx
@@ -10,7 +10,7 @@ export default function OklchDisplay() {
     function handleRemove() {}
 
     return (
-        <div className='px-3'>
+        <div className='flex h-full flex-col px-3'>
             <div className='sticky top-0 flex flex-row items-center justify-between p-2'>
                 <h2 className='text-xl md:text-2xl'>&gt;&nbsp;Color</h2>
                 <div className='mt-1 space-x-3'>
@@ -28,22 +28,27 @@ export default function OklchDisplay() {
                     </button>
                 </div>
             </div>
-            <div className='flex h-full flex-row space-x-3'>
-                <canvas
-                    className='border-primary rounded-box w-1/2 border-1'
-                    style={{
-                        backgroundColor: formatCss(color?.oklch),
-                    }}
-                />
-                <canvas
-                    className='border-primary rounded-box w-1/2 border-1'
-                    style={{
-                        // TODO: Fix canvas display conditions
-                        backgroundColor: formatCss(
-                            clampChroma(color?.oklch, 'oklch'),
-                        ),
-                    }}
-                />
+            <div className='flex h-1/5 flex-row gap-3'>
+                <div>Placeholder; Color Info</div>
+                <div>
+                    <canvas
+                        className='border-primary rounded-box h-full w-full border-1'
+                        style={{
+                            backgroundColor: formatCss(color?.oklch),
+                        }}
+                    />
+                </div>
+                <div>
+                    <canvas
+                        className='border-primary rounded-box h-full w-full border-1'
+                        style={{
+                            // TODO: Fix canvas display conditions
+                            backgroundColor: formatCss(
+                                clampChroma(color?.oklch, 'oklch'),
+                            ),
+                        }}
+                    />
+                </div>
             </div>
         </div>
     )
