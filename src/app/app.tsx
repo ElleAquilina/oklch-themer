cac4ff58944f335a88647f98619494ab9d0f1a5f
eliah <eliah.aquilina@gmail.com>
2025-03-18 15:42:21 -0500

    Style update

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,10 +8,10 @@ function App() {
         <div className='flex h-dvh flex-col'>
             <Header />
             <div className='m-5 flex grow flex-col space-y-5 sm:flex-row sm:space-x-5'>
-                <div className='card bg-base-300 h-1/2 sm:h-[calc(100%-2rem)] sm:w-1/2'>
+                <div className='card bg-base-300 h-1/2 sm:h-[calc(100%-2rem)] sm:w-1/3'>
                     <ColorList />
                 </div>
-                <div className='card bg-base-300 grow sm:h-[calc(100%-2rem)]'>
+                <div className='card bg-base-300 grow sm:h-[calc(100%-2rem)] sm:w-2/3'>
                     <OklchDisplay />
                 </div>
             </div>
