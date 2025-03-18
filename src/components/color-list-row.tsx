95bb2223d3c99b43674292947c7f2d65338bc6d9
eliah <eliah.aquilina@gmail.com>
2025-03-18 09:56:05 -0500

    Break out color list row into own component

---
+++ src/components/color-list-row.tsx
@@ -0,0 +1,133 @@
+import { ColorNameSchema } from '@/schemas/color-name'
+import { colorAtom, colorsAtom } from '@/stores/atoms.tsx'
+import { Color } from '@/types/color.tsx'
+import { clampChroma, formatCss } from 'culori/fn'
+import { motion, Reorder } from 'framer-motion'
+import { useAtom } from 'jotai'
+import { Images, Trash2 } from 'lucide-react'
+import { useState } from 'react'
+import { z } from 'zod'
+import { fromError } from 'zod-validation-error'
+
+export default function ColorListRow({ color }: { color: Color }) {
+    const [colors, setColors] = useAtom(colorsAtom)
+    const [selected, setSelected] = useAtom(colorAtom)
+    const [isEdit, setIsEdit] = useState(false)
+    const [name, setName] = useState('')
+    const [error, setError] = useState('')
+
+    function handleEdit() {
+        setIsEdit(true)
+        setName(color.name)
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setName(e.target.value)
+    }
+
+    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            submit()
+        }
+    }
+
+    function submit() {
+        try {
+            if (name !== color.name) {
+                // TODO: Edge Case; Disallow certain characters, etc.
+                let valid = ColorNameSchema(colors).parse(name)
+
+                if (valid) {
+                    const newColors = colors.map((c) => {
+                        if (c.name === color.name) {
+                            return { ...c, name: name }
+                        } else {
+                            return c
+                        }
+                    })
+
+                    setColors(newColors)
+                }
+            }
+
+            setIsEdit(false)
+            setName('')
+            setError('')
+        } catch (e) {
+            if (e instanceof z.ZodError) {
+                setError(fromError(e).toString())
+            }
+        }
+    }
+
+    function handleDeselect() {
+        setIsEdit(false)
+        setName('')
+        setError('')
+    }
+
+    function handleDuplicate() {}
+    function handleRemove() {
+        // Remove color from color list using name
+        setColors(colors.filter((c) => c.name !== color.name))
+
+        // Remove color from selected
+        if (color.name === selected?.name) {
+            setSelected(null)
+        }
+    }
+
+    return (
+        <Reorder.Item
+            key={color.name}
+            value={color}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={() => setSelected(color)}
+            className={`list-row group mb-1 items-center p-2 ${selected?.name === color.name ? 'bg-base-100' : ''}`}
+        >
+            <div
+                className='badge badge-outline'
+                style={{
+                    backgroundColor: formatCss(clampChroma(color.oklch)),
+                }}
+            />
+            <div>
+                <p onClick={handleEdit} className='inline'>
+                    {isEdit ?
+                        <motion.input
+                            key='input'
+                            id={color.name}
+                            type='text'
+                            value={name}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyDown}
+                            onBlur={handleDeselect}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className={`input input-sm ${error ? 'input-error' : ''}`}
+                        />
+                    :   color.name}
+                </p>
+            </div>
+            <div
+                className={`flex-col space-x-3 opacity-0 transition-opacity duration-400 group-hover:opacity-100 ${isEdit || selected?.name === color.name ? 'opacity-100' : ''}`}
+            >
+                <button
+                    onClick={handleDuplicate}
+                    className='btn btn-circle btn-xs btn-ghost'
+                >
+                    <Images />
+                </button>
+                <button
+                    onClick={handleRemove}
+                    className='btn btn-circle btn-xs btn-ghost'
+                >
+                    <Trash2 />
+                </button>
+            </div>
+        </Reorder.Item>
+    )
+}
--- src/components/color-list.tsx
+++ src/components/color-list.tsx
@@ -1,126 +1,34 @@
+import ColorListRow from '@/components/color-list-row.tsx'
 import { getNearestNamedColor, getRandomOklchColor } from '@/helpers/colors.tsx'
-import { ColorNameSchema } from '@/schemas/color-name.tsx'
-import { colorsAtom, oklchAtom } from '@/stores/atoms.tsx'
+import { colorsAtom } from '@/stores/atoms.tsx'
 import { Color } from '@/types/color.tsx'
-import { clampChroma, formatCss, Oklch } from 'culori/fn'
-import { AnimatePresence, motion, Reorder } from 'framer-motion'
-import { useAtom } from 'jotai/index'
-import { CircleFadingPlus, Images, Trash2 } from 'lucide-react'
-import { useState } from 'react'
-import { z } from 'zod'
-import { fromError } from 'zod-validation-error'
+import { AnimatePresence, Reorder } from 'framer-motion'
+import { useAtom } from 'jotai'
+import { CircleFadingPlus } from 'lucide-react'
 
 export default function ColorList() {
     const [colors, setColors] = useAtom(colorsAtom)
-    const [oklch, setOklch] = useAtom(oklchAtom)
-    const [editColor, setEditColor] = useState<Color | null>(null)
-    const [editName, setEditName] = useState('')
-    const [error, setError] = useState('')
 
     function handleAddColor() {
-        const color: Oklch = getRandomOklchColor()
-        // TODO: Remove edge case: If the same color name is added twice, an error will occur since name is used as list key
-        const name = getNearestNamedColor(color)
+        const oklch = getRandomOklchColor()
+        // TODO: BUG: New color could have same name as an existing color
+        const name = getNearestNamedColor(oklch)
 
-        setColors([
-            ...colors,
-            {
-                name: name,
-                oklch: color,
-            },
-        ])
-        setEditColor(null)
-        setEditName('')
-        setError('')
-    }
-
-    function handleChangeName(e: React.ChangeEvent<HTMLInputElement>) {
-        setEditName(e.target.value)
-    }
-
-    function handleDuplicateColor(color: Color) {
-        // Match names with 'name-XXX' format
-        const extractedName = color.name.match(/^([a-zA-Z]+)-?\d*$/)
-        const name = extractedName ? extractedName[1] : color.name
-        let nameStep = 100
-
-        // Find next available suffix for name (-XXX)
-        // TODO: Remove edge case: 'red-100' is duplicated while 'red' doesnt exist
-        if (colors.find((c) => c.name === name)) {
-            while (colors.find((c) => c.name === `${name}-${nameStep}`)) {
-                nameStep += 100
-            }
+        const newColor: Color = {
+            name: name,
+            oklch: oklch,
         }
 
-        setColors([
-            ...colors,
-            {
-                name: `${name}-${nameStep}`,
-                oklch: color.oklch,
-            },
-        ])
-    }
-
-    function handleEditName(color: Color) {
-        // TODO: Auto-select the text in the input tag
-        setEditColor(color)
-        setEditName(color.name)
-    }
-
-    function handleRemoveColor(color: Color) {
-        // Remove color from color list (color names must be unique)
-        setColors(colors.filter((c) => c.name !== color.name))
-
-        // Remove color from selected colors (if selected)
-        if (color.oklch === oklch) {
-            setOklch(undefined)
-        }
-    }
-
-    function handleSelectColor(color: Color) {
-        setOklch(color.oklch)
-    }
-
-    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
-        if (e.key === 'Enter') {
-            console.log('keydown enter')
-            handleSubmitName()
-        }
-    }
-
-    function handleSubmitName() {
-        console.log('submit of new color on enter key')
-        console.log(editName)
-
-        // TODO: Ensure name does not already exist
-        try {
-            console.log('is valid?')
-            let valid = ColorNameSchema(colors).parse(editName)
-
-            console.log(valid)
-            if (valid) {
-                setError('')
-                // TODO: Use map to update array (cannot be mutated)
-                // https://react.dev/learn/updating-arrays-in-state#replacing-items-in-an-array
-            }
-        } catch (e) {
-            console.log(e)
-            if (e instanceof z.ZodError) {
-                const err = fromError(e)
-                setError(err.toString())
-            }
-        }
-        // TODO: Disallow certain characters
-        // TODO: Throw warning for Tailwind name override
+        setColors([...colors, newColor])
     }
 
     return (
-        <div className='flex h-full flex-col overflow-y-scroll'>
-            <div className='flex flex-row items-center justify-between'>
-                <h2 className='pl-4'>&gt;&nbsp;Color List</h2>
+        <div className='flex h-full flex-col p-3'>
+            <div className='bg-base-300 sticky top-0 z-2 flex flex-row items-center justify-between px-3'>
+                <h2 className='text-xl md:text-2xl'>&gt;&nbsp;Color List</h2>
                 <button
                     onClick={handleAddColor}
-                    className='btn btn-ghost btn-circle mt-1 mr-4'
+                    className='btn btn-ghost btn-circle mt-1'
                     title='Add color'
                 >
                     <CircleFadingPlus />
@@ -129,66 +37,11 @@ export default function ColorList() {
             <Reorder.Group
                 values={colors}
                 onReorder={setColors}
-                className='list grow'
+                className='list grow overflow-y-scroll'
             >
                 <AnimatePresence>
                     {colors.map((color) => (
-                        <Reorder.Item
-                            key={color.name}
-                            value={color}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            className='list-row items-center pb-1'
-                        >
-                            <div
-                                className='badge'
-                                style={{
-                                    backgroundColor: formatCss(
-                                        clampChroma(color.oklch),
-                                    ),
-                                }}
-                            />
-                            <div onClick={() => handleSelectColor(color)}>
-                                {/* TODO: Add prefix dropdown (--, _, -, etc.) */}
-                                <p
-                                    onClick={(e) => e.stopPropagation()}
-                                    onDoubleClick={() => handleEditName(color)}
-                                    className='inline'
-                                >
-                                    {editColor?.name === color.name ?
-                                        <motion.input
-                                            key='input'
-                                            id={color.name}
-                                            type='text'
-                                            value={editName}
-                                            onChange={handleChangeName}
-                                            onKeyDown={handleKeyDown}
-                                            initial={{
-                                                opacity: 0,
-                                            }}
-                                            animate={{ opacity: 1 }}
-                                            transition={{ duration: 0.5 }}
-                                            className='input input-sm'
-                                        />
-                                    :   color.name}
-                                </p>
-                            </div>
-                            <div>
-                                <button
-                                    onClick={() => handleDuplicateColor(color)}
-                                    className='btn btn-circle btn-ghost'
-                                >
-                                    <Images />
-                                </button>
-                                <button
-                                    onClick={() => handleRemoveColor(color)}
-                                    className='btn btn-circle btn-ghost'
-                                >
-                                    <Trash2 />
-                                </button>
-                            </div>
-                        </Reorder.Item>
+                        <ColorListRow color={color} key={color.name} />
                     ))}
                 </AnimatePresence>
             </Reorder.Group>
