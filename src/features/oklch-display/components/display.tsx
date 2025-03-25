b1ba5076bb74f487fc1ad1176858791f4628c87c
eliah <eliah.aquilina@gmail.com>
2025-03-25 02:01:16 -0500

    Added animation to color display

--- src/features/oklch-display/components/display.tsx
+++ src/features/oklch-display/components/display.tsx
@@ -2,12 +2,16 @@ import { inRgb } from '@/helpers/colors.tsx'
 import { colorAtom } from '@/stores/atoms.tsx'
 import { formatCss } from 'culori'
 import { clampChroma } from 'culori/fn'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useAtom } from 'jotai'
 import { Copy } from 'lucide-react'
 
 export default function Display() {
     const [color, setColor] = useAtom(colorAtom)
+
+    const isUnselected = !color
+    const isValid = !!(color && inRgb(color))
+    const isUnavailable = !!(color && !inRgb(color))
 
     /**
      * Set the selected color to the fallback color (an OKLCH color that
@@ -22,71 +26,90 @@ export default function Display() {
 
     return (
         <>
-            <div className='flex h-3/10 w-full flex-row gap-1 text-sm'>
-                {/* Color Displays */}
-                {color && inRgb(color) ?
-                    <>
-                        <motion.div
-                            className='rounded-box border-neutral h-full w-full border-1 md:w-1/2'
-                            style={{ backgroundColor: formatCss(color) }}
-                            initial={{
-                                width: 0,
-                                backgroundColor: formatCss(color),
-                            }}
-                            animate={{
-                                width: '100%',
-                                backgroundColor: formatCss(color),
-                            }}
-                            transition={{
-                                width: { duration: 0.5 },
-                                backgroundColor: { duration: 0.5 },
-                            }}
-                        />
-                    </>
-                :   <>
-                        <div className='rounded-box border-neutral flex h-full w-full justify-center border-2 border-dotted'>
-                            <div>
-                                <button className='btn btn-disabled btn-xs m-2'>
-                                    {color ?
-                                        'unavailable'
-                                    :   'no color selected'}
-                                </button>
-                            </div>
-                        </div>
-                    </>
-                }
-                {/* Additional Color Display: RGB Out of Range */}
-                {color && !inRgb(color) ?
-                    <>
-                        <motion.div
-                            className='rounded-box border-neutral flex h-full w-full justify-center border-1 md:w-1/2'
-                            style={{
-                                backgroundColor: formatCss(clampChroma(color)),
-                            }}
-                            initial={{
-                                width: 0,
-                                backgroundColor: formatCss(clampChroma(color)),
-                            }}
-                            animate={{
-                                width: '100%',
-                                backgroundColor: formatCss(clampChroma(color)),
-                            }}
-                            transition={{
-                                width: { duration: 0.5 },
-                                backgroundColor: { duration: 0.5 },
-                            }}
-                        >
-                            <div>
-                                <button
-                                    onClick={handleFallback}
-                                    className={`btn btn-xs m-2`}
+            <div className='flex h-3/10 w-full flex-row justify-between gap-1 text-sm'>
+                <AnimatePresence>
+                    <motion.div
+                        key='modal'
+                        initial={{
+                            backgroundColor:
+                                isValid ?
+                                    formatCss(color)
+                                :   'oklch(0% 0 0 / 0%)',
+                        }}
+                        animate={{
+                            backgroundColor:
+                                isValid ?
+                                    formatCss(color)
+                                :   'oklch(0% 0 0 / 0%)',
+                        }}
+                        exit={{
+                            backgroundColor:
+                                isValid ?
+                                    formatCss(color)
+                                :   'oklch(0% 0 0 / 0%)',
+                        }}
+                        transition={{ duration: 1 }}
+                        className='rounded-box border-neutral flex h-full w-full justify-center border-1'
+                    >
+                        <div>
+                            {(isUnselected || isUnavailable) && (
+                                <motion.button
+                                    key='modal-close'
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 100 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 1 }}
+                                    className='btn btn-disabled btn-xs m-2'
                                 >
-                                    fallback
-                                </button>
-                            </div>
-                        </motion.div>
-                    </>
-                :   ''}
+                                    {isUnselected && 'no color selected'}
+                                    {isUnavailable && 'unavailable'}
+                                </motion.button>
+                            )}
+                        </div>
+                    </motion.div>
+
+                    {/* Fallback Canvas */}
+                    {color && !inRgb(color) ?
+                        <>
+                            <motion.div
+                                className='rounded-box border-neutral flex h-full w-full justify-center border-1 md:w-1/2'
+                                style={{
+                                    backgroundColor: formatCss(
+                                        clampChroma(color),
+                                    ),
+                                }}
+                                initial={{
+                                    width: 0,
+                                    backgroundColor: formatCss(
+                                        clampChroma(color),
+                                    ),
+                                }}
+                                animate={{
+                                    width: '100%',
+                                    backgroundColor: formatCss(
+                                        clampChroma(color),
+                                    ),
+                                }}
+                                transition={{
+                                    duration: 2,
+                                }}
+                                exit={{
+                                    width: 0,
+                                    originX: 0,
+                                }}
+                            >
+                                <div>
+                                    <button
+                                        onClick={handleFallback}
+                                        className={`btn btn-xs m-2`}
+                                    >
+                                        fallback
+                                    </button>
+                                </div>
+                            </motion.div>
+                        </>
+                    :   ''}
+                </AnimatePresence>
             </div>
 
             {/* Color Information */}
