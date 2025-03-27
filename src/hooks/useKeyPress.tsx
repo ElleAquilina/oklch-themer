0a9b347ec915439d7e6fa8d00759d57978a99e2c
eliah <eliah.aquilina@gmail.com>
2025-03-26 22:30:07 -0500

    Fix bug with keypress hook

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 export function useKeyPress(callback: () => void, keycodes: string[]) {
     function handler(e: KeyboardEvent) {
         if (keycodes.includes(e.key)) {
+            e.preventDefault()
             callback()
         }
     }
