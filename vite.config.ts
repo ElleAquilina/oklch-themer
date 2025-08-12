08d1af7ed9ed35a3cc0befd763c32fcbeff2aa47
eliah <eliah.aquilina@gmail.com>
2025-08-12 14:29:35 -0500

    Update package lock and vite config for github pages (custom domain!)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/oklch-themer/', // for github pages
+    base: '/', // for github pages
     resolve: {
         alias: [
             {
